6788347c721bdf933a3db614bd3c6e31e0a231e2
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-01-09 19:44:25 -0800

    first commit

---
+++ fundamentals/first_java_program/Uno.java
@@ -0,0 +1,5 @@
+public class Uno {
+    public static void main(String[] args) {
+        System.out.println("My name is Samuel Rodriguez \nI am 20 years old \nMy hometown is Spring Valley, CA");
+    }
+}
