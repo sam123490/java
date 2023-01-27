04d3b371d2c5d8c192c2507d1e47c441aeccbbc5
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-01-26 19:16:06 -0800

    interfaces and extending classes

--- OOP/careSoftInterfaces/src/com/caresoft/clinicapp/AdminUser.java
+++ OOP/careSoftInterfaces/src/com/caresoft/clinicapp/AdminUser.java
@@ -1,9 +1,5 @@
 package com.caresoft.clinicapp;
 
-public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
-
-	public AdminUser() {
-		// TODO Auto-generated constructor stub
-	}
+public class AdminUser implements HIPAACompliantUser, HIPAACompliantAdmin {
 
 }
--- OOP/careSoftInterfaces/src/com/caresoft/clinicapp/Physician.java
+++ OOP/careSoftInterfaces/src/com/caresoft/clinicapp/Physician.java
@@ -1,17 +1,44 @@
 package com.caresoft.clinicapp;
+import java.sql.Date;
+import java.util.ArrayList;
 
 public class Physician extends User implements HIPAACompliantUser {
+	  private ArrayList<String> patientNotes;
 	
 	public Physician(Integer id) {
 		super(id);
-		// TODO Auto-generated constructor stub
 	}
 	
-	public boolean assignPIN(int pin) {
-		if(pin.length == 4) {
-			
+	public boolean assignPin(int pin) {
+		int numDigits = String.valueOf(pin).length();
+		if(numDigits == 4) {
+			setPin(pin);
+			return true;
 		}
-		this.pin = pin;
+		else {
+			return false;
+		}
 	}
 	
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		if(getId() == confirmedAuthID) {			
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+    public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+    
+    public String getPatientNote(int index){
+    	return patientNotes.get(index);
+    }
 }
