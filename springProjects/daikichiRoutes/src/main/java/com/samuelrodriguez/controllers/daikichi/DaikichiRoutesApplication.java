48dffe2b29351f69db5086e2ddc867395879f4b6
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-01-28 15:24:28 -0800

    using path variables

--- springProjects/daikichiRoutes/src/main/java/com/samuelrodriguez/controllers/daikichi/DaikichiRoutesApplication.java
+++ springProjects/daikichiRoutes/src/main/java/com/samuelrodriguez/controllers/daikichi/DaikichiRoutesApplication.java
@@ -2,6 +2,7 @@ package com.samuelrodriguez.controllers.daikichi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,27 @@ public class DaikichiRoutesApplication {
 		@RequestMapping("/tomorrow")
 		public String tomorrow() {
 			return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+		}
+		
+		@RequestMapping("/travel/{place}")
+		public String travel(@PathVariable("place") String place) {
+			return "Congratulations! You will soon travel to " + place + "!";
+		}
+		
+		@RequestMapping("/lotto/{num}")
+		public String fortune(@PathVariable("num") String num) {
+			int number = Integer. parseInt(num);
+			if (number % 2 == 0) {
+				return "You will take a grand journey in the near future,"
+						+ "but be weary of tempting offers";
+			}
+			else if(number % 2 == 1) {
+				return "You have enjoyed the fruits of your labor but now is "
+						+ "a great time to spend time with family and friends";
+			}
+			else {
+				return "https://www.youtube.com/watch?v=coyMil9mV9o";
+			}
 		}
 	}
 	
